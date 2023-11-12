7418621043dbefe2cfe74302477f232d82d304f4
uttam-kumar-das <duttam800@gmail.com>
2023-11-12 16:34:20 +0530

    Test changes

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -2,11 +2,13 @@
 
 package com.uttam;
 
+
+
 public class FibonacciSeries {
 
 	public static void main(String[] args) {
 		int x=0,y=1,z=0;
-		for(int i=1; i<=10;i++) {//Here loop is running 10 times
+		for(int i=1; i<=10;i++) {//Here loop is running 10 times 
 			z=x+y;
 			System.out.print(z+" ");
 			x=y;
