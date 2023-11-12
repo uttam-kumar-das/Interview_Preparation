c33e99c8e75be307707dc3570e8e150ccdd9df14
uttam-kumar-das <duttam800@gmail.com>
2023-11-12 12:15:46 +0530

    first commit

---
+++ FibonacciSeries.java
@@ -0,0 +1,18 @@
+//Printing Fibonacci series
+
+package com.uttam;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		int x=0,y=1,z=0;
+		for(int i=1; i<=10;i++) {//Here loop is running 10 times
+			z=x+y;
+			System.out.print(z+" ");
+			x=y;
+			y=z;
+		}
+		
+	}
+
+}
